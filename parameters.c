10e5022bff2f316d068bc0fb2bb1b3c223054cb0
Давыдов <den2011chs8@gmail.com>
2021-02-15 22:23:23 +0000

    Удалён код от http наследия

--- parameters.c
+++ parameters.c
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include "pins_arduino.h"
 
+#ifdef WITH_STRING
 static int m_add_to_string(char* stringPtr, char *format, ...)
 {
   va_list ptr;
@@ -11,6 +12,7 @@ static int m_add_to_string(char* stringPtr, char *format, ...)
   va_end(ptr);
   return (strlen(stringPtr));
 }
+#endif
 
 
 #ifdef WITH_STRING
@@ -23,23 +25,10 @@ static char m_buffer_for_string [MAX_REQUEST_STRING_LEN];
 static void m_make_string(const struct st_parameters *params);
 #endif
 
-#ifdef WITH_TIME
-static void m_update_time(struct st_parameters *params);
-#endif
-
 /*****************************************************************/
 
 void Parametrist_setup(struct  st_parameters *params)
 {
-  #ifdef WITH_TIME
-  /*настраиваем время, например: 12 НОЯ 1955 22ч 3м 30 сек. */
-  params->curr_date_time.day = 12;
-  params->curr_date_time.month = 11;
-  params->curr_date_time.year = 1955;
-  params->curr_date_time.hour = 22;
-  params->curr_date_time.minute = 3;
-  params->curr_date_time.second = 30;
-  #endif 
   params->is_eq = 2;
   params->is_reg = 5;
   Parametrist_update(params);
@@ -47,10 +36,7 @@ void Parametrist_setup(struct  st_parameters *params)
 
 int8_t Parametrist_update(struct  st_parameters *params)
 {
-  #ifdef WITH_TIME
-  m_update_time(params);
-  #endif
-  
+
   params->i_a = analogRead(A6);
   params->i_b = analogRead(A5);
   params->i_c = analogRead(A4);
@@ -83,17 +69,8 @@ const char* Parametrist_HTTP_string(void)
 */
 static void m_make_string(const struct st_parameters *params)
 {
-  #ifdef WITH_TIME
-  int offset = m_add_to_string((char*)(m_buffer_for_string), "{\"consumData\": { \"dt\":\"");
-  offset += m_add_to_string((char*)(m_buffer_for_string + offset),  "%u-%u-%uT%u:%u:%u,",
-          params->curr_date_time.year, params->curr_date_time.month, params->curr_date_time.day,
-          params->curr_date_time.hour, params->curr_date_time.minute, params->curr_date_time.second
-  );
-  #else
-
   int offset = m_add_to_string((char*)(m_buffer_for_string), "{\"consumData\": { \"dt\":\"");
   offset += m_add_to_string((char*)(m_buffer_for_string + offset),  ",");
-  #endif 
   
   offset += m_add_to_string((char*) ( m_buffer_for_string + offset ),  "\"Eq\":\"%d\",", params->is_eq );
   offset += m_add_to_string((char*) (m_buffer_for_string + offset),  "\"Reg\":\"%d\",", params->is_reg);
@@ -105,23 +82,3 @@ static void m_make_string(const struct st_parameters *params)
   offset += m_add_to_string((char*) (m_buffer_for_string + offset), "\"V_C\":\"%d\"}}\0", params->v_c);
 }
 #endif
-/**
-   Обновляем время (сейчас просто имитация).
-*/
-#ifdef WITH_TIME
-static void m_update_time(struct st_parameters *params)
-{
-  ++params->curr_date_time.second;
-  if (params->curr_date_time.second >= 60)
-  {
-    params->curr_date_time.second = 0;
-    ++params->curr_date_time.minute;
-  }
-  if (params->curr_date_time.minute >= 60)
-  {
-    params->curr_date_time.minute = 0;
-    ++params->curr_date_time.hour;
-  }
-  //ну, и т.д.
-}
-#endif // WITH_TIME
--- parameters_datatypes.h
+++ parameters_datatypes.h
@@ -4,7 +4,6 @@
 #include <stdint.h>
 
 //#define WITH_STRING
-//#define WITH_TIME
 
 #ifdef __cplusplus
 extern "C" {
@@ -14,26 +13,10 @@ extern "C" {
 #pragma pack(push, 1)
 /* время */
 
-#ifdef WITH_TIME
-struct st_curr_time
-{
-  uint16_t year;
-  uint8_t month;
-  uint8_t day;
-  
-  uint8_t hour;
-  uint8_t minute;
-  uint8_t second;
-};
-#endif 
-
 /*Параметры для мониторинга */
 
 typedef struct  st_parameters
 {
-  #ifdef WITH_TIME
-  struct st_curr_time curr_date_time;
-  #endif
   uint8_t is_eq;
   uint8_t is_reg;
   
