d9436600c321e8247001fba54142490fa0421a15
Давыдов <den2011chs8@gmail.com>
2021-02-15 22:23:48 +0000

    новая сетевая библиотека

--- NanoUDPClient.ino
+++ NanoUDPClient.ino
@@ -1,39 +1,32 @@
-#include <EtherCard.h>
+#include <UIPEthernet.h>
 #include <avr/wdt.h>
 #include "parameters.h"
 
-#define PLAT_NUM 1  /* номер платы */
-#define LED 13      /* Пин светодиода. */
-#define BUFF_SIZE 340 /* Размер буфера Ethernet. Если меньше 334 - не принимает сервер*/
+#define PLAT_NUM 1  /* Номер платы */
+#define LED 13      /* Пин светодиода */
 
-//const char SERVER_IP[] PROGMEM = "192.168.3.101";
-//const char SERVER_IP[] PROGMEM = "172.31.54.147";
-//const char SERVER_IP[] PROGMEM = "172.22.3.191";
-//const char SERVER_IP[] PROGMEM = "retc.vniizht.lan";
-//const char SERVER_DNS[] = "vniizht.lan";
-//const char SERVER_DNS[] = "172.31.54.7";
-//const char SERVER_DNS[] = "172.31.54.147";
-const char SERVER_DNS[] = "0.0.0.0";
+EthernetUDP udp;
 
-//const char SERVER_DNS[] = "192.168.3.101";
-
-//const int srcPort  = (1000 + PLAT_NUM);
-//const int dstPort  = 3000;
+//#define SERVER_IP 192,168,3,51   /* IP Адрес назначения */
+#define SERVER_IP 172,31,54,147   /* IP Адрес назначения */
+//#define PORT_SRC  (1000 + PLAT_NUM) /* Порт отправки */
+#define PORT_DST  2000              /* Порт назначения */
 
 /* коды ошибок */
-#define ETHERNET_ERROR 3 // Failed to access Ethernet controller
-#define DHCP_ERROR     4 // DHCP failed
-#define DNS_ERROR      5 // DNS lookup failed for the apihost
+#define ETHERNET_ERROR 3  /* Не удалось инициализировать Ethernet */
+#define SEND_DELAY     1  /* Через какое время повторять отправку */
+#define WAIT_SEND      5000 /* Ожидание отправки дейтаграммы */
 
-uint32_t  g_timeMS = 0;
-struct    st_parameters g_main_parameters; /* Структура с параметрами. В ней всё хранится */
-const byte mymac[] = { 0x74, 0x69, 0x69, 0x2D, 0x30, 0x32 };
-byte Ethernet::buffer[BUFF_SIZE];
+uint32_t   g_timeMS = 0;
+struct     st_parameters g_main_parameters; /* Структура с параметрами. В ней всё хранится */
+const byte mymac[] PROGMEM = { 0x74, 0x69, 0x69, 0x2D, 0x30, 0x32 };
 
-/* местные функции */
-int8_t init_ethernet();
+/**** местные функции ****/
+int8_t init_ethernet(void);
 void emergency(uint8_t errnum, uint8_t blockCount);
 uint8_t resetFlags __attribute__ ((section(".noinit")));
+/*************************/
+
 
 /*Для работы с WatchDog*/
 uint8_t resetFlagsInit (void) __attribute__ ((naked))
@@ -45,36 +38,34 @@ uint8_t resetFlagsInit (void)
   __asm__ __volatile__ ("sts %0, r2\n" : "=m" (resetFlags) :);
 }
 
-
 void check_watch_dog(void)
 {
-
   if (resetFlags & (1 << WDRF))
   {
      resetFlags &= ~(1 << WDRF);
-     Serial.println(" Watchdog");
+     Serial.println(F("Watchdog"));
   }
 
   if (resetFlags & (1 << BORF))
   {
      resetFlags &= ~(1 << BORF);
-     Serial.println(" Brownout");
+     Serial.println(F("Brownout"));
   }
   if (resetFlags & (1 << EXTRF))
   {
      resetFlags &= ~(1 << EXTRF);
-     Serial.println(" External");
+     Serial.println(F("External"));
   }
   if (resetFlags & (1 << PORF))
   {
      resetFlags &= ~(1 << PORF);
-     Serial.println(" PowerOn");
+     Serial.println(F("PowerOn"));
   }
   if (resetFlags != 0x00)
   {
-     Serial.println(" Unknown");
+     Serial.println(F("Unknown"));
   }
-  Serial.println("");
+  //Serial.println("");
 }
 
 void setup (void)
@@ -82,76 +73,66 @@ void setup (void)
     Serial.begin(9600);
     check_watch_dog();
     Parametrist_setup(&g_main_parameters);
-    Serial.println(F("initializing ethernet..."));
+    Serial.println(F("Init ethernet..."));
     if ( init_ethernet() != 0 )
     {   
-        Serial.println(F("Reboot after 2 seconsd"));
+        Serial.println(F("Ethernet Fail. Reboot after 2 sec..."));
         wdt_enable(WDTO_2S);
         delay(4000);
     }
+    Serial.println(F("Ethernet OK"));
     g_timeMS = millis();
     wdt_enable(WDTO_4S);
 }
 
-
 void loop (void)
 {   
     wdt_reset();
+    int success;
     if (millis() > g_timeMS )
     { 
      Parametrist_update(&g_main_parameters);
-     Serial.println(F("Sending..."));
+     
+     do
+        {
+          //beginPacket fails if remote ethaddr is unknown. In this case an
+          //arp-request is send out first and beginPacket succeeds as soon
+          //the arp-response is received.
+          success = udp.beginPacket(IPAddress(SERVER_IP), PORT_DST);
+          Serial.println(success ? F("OK") : F("Fail"));
+        }
+      while (!success && ((long)(millis() - WAIT_SEND ))<0);
+      if (!success )
+        goto stop;
 
-     int srcPort  = (1000 + PLAT_NUM);
-     int dstPort  = 2000;
-     uint8_t ipDestinationAddress[IP_LEN];
-
-     // 172.22.3.204
-     // 192.168.4.75
-     // 172.31.54.147
- 
-     ipDestinationAddress[0] =  172;
-     ipDestinationAddress[1] =  31;
-     ipDestinationAddress[2] =  54;
-     ipDestinationAddress[3] =  147;
-      
-     ether.sendUdp((const char*)(&g_main_parameters), 
-              sizeof(g_main_parameters), srcPort, ipDestinationAddress, dstPort );
-     g_timeMS = millis() + 1000;
+      Serial.println(F("Sending..."));
+      success = udp.write((const char*)(&g_main_parameters), sizeof(g_main_parameters));
+      if (!success )
+        goto stop;
+      success = udp.endPacket();
+      if (!success)
+         goto stop;
+      //Serial.println(success ? F("success") : F("failed"));
+      success = 1;
+      stop:
+      if (!success)
+        Serial.print(F("Send failed"));
+      udp.stop();
+      g_timeMS = millis() + SEND_DELAY;
     }
 }
 
-/*инициализация сетевой карточки. 
+/*
+ * Инициализация сетевой карточки. 
  * Возвращает 0, в случае успешной инициализации , или код ошибки.
 */
-int8_t init_ethernet()
-{
-    if (ether.begin(sizeof Ethernet::buffer, mymac, SS) == 0) 
+int8_t init_ethernet(void)
+{ 
+    if (Ethernet.begin( mymac) == 0) 
     {
       emergency(ETHERNET_ERROR, 0);
-      Serial.println(F("ethernet fail"));
       return ETHERNET_ERROR;
     }
-    Serial.println(F("ethernet OK"));
-
-    if (!ether.dhcpSetup())
-    {
-      emergency(DHCP_ERROR, 0);
-      Serial.println(F("DHCP fail"));
-      return DHCP_ERROR;
-    }
-    Serial.println(F("DHCP OK"));
-    
-    ether.printIp("IP:   ", ether.myip); // output IP address to Serial
-    ether.printIp("GW:   ", ether.gwip); // output gateway address to Serial
-    ether.printIp("Mask: ", ether.netmask); // output netmask to Serial
-    ether.printIp("DHCP server: ", ether.dhcpip); // output IP address of the DHCP server
-  
-    if (!ether.dnsLookup(SERVER_DNS))
-      Serial.println(F("DNS fail"));
-    else
-      Serial.println(F("DNS OK"));
-  
     return 0;
 }
 
