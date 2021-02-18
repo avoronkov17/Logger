cad1707354321768b1a6306743c5eb0ea6489fb0
DoctorSnp <den2011chs8@gmail.com>
2021-02-18 18:19:22 +0300

    добавлен файл для хранения настроек

---
+++ settings.h
@@ -0,0 +1,18 @@
+#ifndef REGISTRATOR_SETTINGS_H
+#define REGISTRATOR_SETTINGS_H
+
+/**
+ *  Настройки под конкретную плату
+ */
+
+#define PLAT_NUM 1  /* Номер платы */
+//#define SERVER_IP 192,168,3,51   /* IP Адрес назначения */
+#define SERVER_IP 172,31,54,147   /* IP Адрес назначения */
+
+#define PORT_DST  2000              /* Порт назначения */
+
+//#define PORT_SRC  (1000 + PLAT_NUM) /* Порт отправки. Пока не нужно*/
+#define SEND_DELAY     1000  /* Через какое время повторять отправку */
+
+
+#endif // REGISTRATOR_SETTINGS_H
